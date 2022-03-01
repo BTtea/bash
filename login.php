075a684a9b20c6c47af8bf734665f7a35d279494
fzwww <box4074@gmail.com>
2022-03-01 21:59:24 +0800

    3/1 21:59

--- login.php
+++ login.php
@@ -9,12 +9,18 @@ $statement->execute(array(
 ));
 $row = $statement->fetch(PDO::FETCH_ASSOC);
 if (is_array($row)) {
+    session_start();
+    $_SESSION['loggedIn'] = true;
+    $_SESSION['id'] = $row['id'];
+    $_SESSION['name'] = $userName;
     echo "登入成功, 歡迎$userName";
-}else{
+    echo "<p/><a href='logout.php'>登出</a>";
+} else {
     alert('帳號或密碼錯誤, 請重新輸入');
 }
 
-function alert($msg){
+function alert($msg)
+{
     echo "<script>alert('$msg');
      window.location.href='index.html';
     </script>";
